812bb6be8a36c2ec970b5b9a36578a32f1fcc715
BotAssist.org <botassist.org@gmail.com>
2026-07-05 12:49:16 +0600

    Create data.js

---
+++ api/data.js
@@ -0,0 +1,32 @@
+const APPS_SCRIPT_URL =
+  "https://script.google.com/macros/s/AKfycbyeqU4ymdxnzjobWfzWiVIaVb8zZtKgn5JIx-PF6oW6rWeZh4XocpmaQKOiMMCumJ8zPA/exec";
+
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method not allowed" });
+  }
+
+  try {
+    const { licenseKey, page } = req.body;
+
+    const response = await fetch(APPS_SCRIPT_URL, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        action: "GET_DATA",
+        licenseKey: licenseKey,
+        page: page
+      }),
+      redirect: "follow"
+    });
+
+    const data = await response.json();
+    return res.status(200).json(data);
+
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      error: "API_ERROR: " + error.message
+    });
+  }
+}
